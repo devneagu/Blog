d8f94e6c8ac2633d34ee90fe3ff22662e314915d
NeaguMiha <72192476+NeaguMiha@users.noreply.github.com>
2021-07-06 14:26:46 +0300

    g

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: "Myasd New Blog",
+    description: "This is my awesome blog I made from scratch!",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
+}
---
+++ src/blog/second-post.md
@@ -0,0 +1,9 @@
+---
+title: Second
+author: Nikola
+date: 2020-07-15
+---
+
+## Introduction to my blog post
+
+Great content of my first blog
